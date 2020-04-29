b3ba2f43602975ff22f63600cb3fc3066b323986
André Mendes Marcondes <andremendesmarcondes@gmail.com>
2020-04-29 01:20:55 -0300

    Add project files.

---
+++ JWTCore3/Data/DemoTokenContext.cs
@@ -0,0 +1,17 @@
+﻿using JWTCore3.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTCore3.Data
+{
+    public class DemoTokenContext : IdentityDbContext
+    {
+        public DemoTokenContext(DbContextOptions<DemoTokenContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
